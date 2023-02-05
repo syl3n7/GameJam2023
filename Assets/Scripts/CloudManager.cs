62e95a57794a82393391d0f67c234246d175531c
syl3n7 <a040516@ipmaia.pt>
2023-02-05 15:02:17 +0000

    nuvens automaticas
    :)

--- Assets/Scripts/CloudManager.cs
+++ Assets/Scripts/CloudManager.cs
@@ -9,6 +9,8 @@ public class CloudManagerScript : MonoBehaviour {
     //Set this variable to how often you want the Cloud Manager to make clouds in seconds.
     //For Example, I have this set to 2
     public float delay;
+
+    public Transform parentOfPrefab;
  
     //If you ever need the clouds to stop spawning, set this variable to false, by doing: CloudManagerScript.spawnClouds = false;
     public static bool spawnClouds = true;
@@ -25,7 +27,8 @@ public class CloudManagerScript : MonoBehaviour {
             //Only spawn clouds if the boolean spawnClouds is true
             while(spawnClouds) {
                 //Instantiate Cloud Prefab and then wait for specified delay, and then repeat
-                Instantiate(cloudPrefab); // colocar o parent correto @serra
+                //Instantiate(cloudPrefab); // colocar o parent correto @serra
+                Instantiate(cloudPrefab, parentOfPrefab);
                 yield return new WaitForSeconds(delay);
             }
         }
